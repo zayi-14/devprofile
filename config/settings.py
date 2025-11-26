cd3fd3cffe068c4d7a0194bc437c582fa5aa4119
zayi-14 <abhinavpcofficial@gmail.com>
2025-11-26 16:53:01 +0530

    Updatedn

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ─────────────────────────────────────
 SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-dev-key")
 DEBUG = os.environ.get("DEBUG", "True") == "True"
-ALLOWED_HOSTS = [devprofile-xi.vercel.app,.vercel.app,localhost,127.0.0.1,]
+ALLOWED_HOSTS = [".vercel.app", "localhost", "127.0.0.1"]
+
 
 # ─────────────────────────────────────
 # APPS
